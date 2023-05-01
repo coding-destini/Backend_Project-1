fa4d66b7bbf1b54ac7ee2bf3052348925476e6d3
Akash Shah <akashshahngu@gmail.com>
2023-05-01 08:35:31 +0530

    Create Token for User Sign In and Get the token in POSTMAN

---
+++ controller/api/v1/user_api.js
@@ -0,0 +1,29 @@
+const User  = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+
+module.exports.createSession = async (req,res)=>{
+// 1: What we do when a user name and password is recieved => we need to find that user and generate the jwt.JsonWebToken 
+// corresponding to that user 
+try {
+    let user =await User.findOne({email : req.body.email});
+    //If user not found
+    if(!user || user.password!=req.body.password){
+        return res.json(422,{
+            message:"Invalid UserName of Password"
+        })
+    }
+    // If uesr found 
+    return res.json(200,{
+        message:"Sign in SuccessFully , here is your token , please keep it safe",
+        data:{
+            token : jwt.sign(user.toJSON(),'CodeBook',{expiresIn:'10000'}) //jwt.sign() is a fun() where i will pass my user and convert it to Json , and the key which we created in passport-jwt to encrypt , same key should be here to decrypt || 10000 is milisecond
+        }
+    })
+
+} catch (error) {
+    console.log("error ---- ",error)
+    return res.json(400,{
+      message:"Internal"
+    })
+}
+}
